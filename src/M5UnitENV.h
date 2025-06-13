6e2d389059a21c7bc5a459ce37f7c2920ab54e17
dxmnkd316 <46753733+dxmnkd316@users.noreply.github.com>
2025-06-13 08:58:49 -0500

    Update M5UnitENV.h

--- src/M5UnitENV.h
+++ src/M5UnitENV.h
@@ -6,11 +6,11 @@
 #define _M5_UNIT_ENV_H_
 
 #include "Arduino.h"
-#include "DHT12.h"
-#include "QMP6988.h"
-#include "SHT3X.h"
+//#include "DHT12.h"
+//#include "QMP6988.h"
+//#include "SHT3X.h"
 #include "SHT4X.h"
 #include "BMP280.h"
-#include "SCD4X.h"
+//#include "SCD4X.h"
 
 #endif
