8c123b91ae4baa7317f176cdc9a25e03fa30be44
dxmnkd316 <46753733+dxmnkd316@users.noreply.github.com>
2025-09-26 22:19:35 -0500

    Update SCD41m5.h

--- src/SCD41m5.h
+++ src/SCD41m5.h
@@ -1,1 +1,13 @@
+#pragma once
+#ifdef SENSORS
+#include <ArduinoJson.h>
 
+namespace SCD41m5 {
+void ConnectToWifi();
+void SerialReport();
+bool SendDiscovery();
+void Setup();
+void Loop();
+}  // namespace SCD41m5
+
+#endif
